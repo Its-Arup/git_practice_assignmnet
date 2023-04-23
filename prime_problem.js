70aaf20037740cd91999e48744c36db030fcd16d
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-23 20:11:36 +0530

    Code fixed. Changed loop termination conditin to less than equal to. Changed if condition from == 5 to == 2.

--- prime_problem.js
+++ prime_problem.js
@@ -1,11 +1,11 @@
-let number =13;
+let number = 13;
 let factor = 0; 
-for(let i=1;i<number;i++){
+for(let i = 1;i <= number; i++){
     if(number%i==0){
         factor++
     }
 }
-if(factor==5){
+if(factor == 2){
     console.log("This is a prime number");
 }else {
     console.log("This is not a prime number");
