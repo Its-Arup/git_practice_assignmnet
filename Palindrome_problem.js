30b2d28d11862d3698a318c1e0dd7ec628e156c3
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-23 20:12:15 +0530

    Code fixed. Changed if condition from just equals to equal equal to.

--- Palindrome_problem.js
+++ Palindrome_problem.js
@@ -1,10 +1,10 @@
-let str="nayan";
+let str = "nayana";
 let bag=""
-for(let i=str.length-1;i>=0;i--){
-    bag+=str[i];
+for(let i = str.length-1;i>=0;i--){
+    bag += str[i];
 }
-if(bag=str){
-    console.log(str," is a palindrome")
+if(bag == str){
+    console.log(str,"is a palindrome")
 }else{
-    console.log(str," is not a palindrome")
+    console.log(str,"is not a palindrome")
 }
