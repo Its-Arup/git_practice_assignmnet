ca53b3872cf27e9d8dc8f7e4c327d12567c699e3
unknown <arupaul1650@gmail.com>
2023-04-23 17:48:47 +0530

    prime_problem

---
+++ prime_problem.js
@@ -0,0 +1,12 @@
+let number =13;
+let factor = 0; 
+for(let i=1;i<number;i++){
+    if(number%i==0){
+        factor++
+    }
+}
+if(factor==5){
+    console.log("This is a prime number");
+}else {
+    console.log("This is not a prime number");
+}
